728de6f28a2fc30a0c3e57d7a60f01c0ba859950
AdlinBebisha <146411432+AdlinBebisha@users.noreply.github.com>
2023-10-23 19:32:15 +0530

    Add files via upload

---
+++ AI_Phase4/RandomForest.py
@@ -0,0 +1,40 @@
+# Import necessary libraries
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, roc_auc_score, confusion_matrix
+
+# Load the Kaggle diabetes dataset
+data = pd.read_csv('diabetes.csv')
+
+# Split the data into features (X) and target (y)
+X = data.drop('Outcome', axis=1)  # 'Outcome' is the target variable
+y = data['Outcome']
+
+# Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Create a RandomForestClassifier
+model = RandomForestClassifier(n_estimators=100, random_state=42)
+
+# Train the model on the training data
+model.fit(X_train, y_train)
+
+# Make predictions on the test data
+y_pred = model.predict(X_test)
+
+# Evaluate the model's performance
+accuracy = accuracy_score(y_test, y_pred)
+precision = precision_score(y_test, y_pred)
+recall = recall_score(y_test, y_pred)
+f1 = f1_score(y_test, y_pred)
+roc_auc = roc_auc_score(y_test, y_pred)
+confusion = confusion_matrix(y_test, y_pred)
+
+# Output the results
+print("Accuracy:", accuracy)
+print("Precision:", precision)
+print("Recall:", recall)
+print("F1 Score:", f1)
+print("ROC AUC Score:", roc_auc)
+print("Confusion Matrix:\n", confusion)
